fba32d95cda9fce1126b45de905af78d5c2155e3
zmijevik <persie.joseph@gmail.com>
2013-03-10 23:02:25 -0500

    fixing the environment scripts

--- web/index_dev.php
+++ web/index_dev.php
@@ -1,7 +1,6 @@
 <?php
 
-
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('inmates', 'prod', true);
+$configuration = ProjectConfiguration::getApplicationConfiguration('inmates', 'dev', true);
 sfContext::createInstance($configuration)->dispatch();
